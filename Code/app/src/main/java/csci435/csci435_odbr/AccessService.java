6a14d46f160fa4244a422fe8cfe3fccf52000707
bwotten <bwotten@email.wme.edu>
2016-02-18 19:08:41 -0500

    Only relative data goes to the Bug Report

--- Code/app/src/main/java/csci435/csci435_odbr/AccessService.java
+++ Code/app/src/main/java/csci435/csci435_odbr/AccessService.java
@@ -14,14 +14,9 @@ public class AccessService extends AccessibilityService{
     public void onAccessibilityEvent(AccessibilityEvent event) {
 
         if(event.getPackageName().equals(Globals.packageName)) {
-            AccessibilityNodeInfo nodeInfo = event.getSource();
-            Toast.makeText(getBaseContext(), "Service: " + event.getPackageName(), Toast.LENGTH_SHORT).show();
-            //Where do we store this data? It all seems pertinent, only keep data that is not part of our app so we ignore things
-            //That have packageName == "csci435.csci435_odbr"
-            event.getEventType();
-            event.getPackageName();
-            event.getEventTime();
-            //record event and take snapshot here?
+
+            BugReport.getInstance().addUserEvent(event);
+
         }
     }
 
