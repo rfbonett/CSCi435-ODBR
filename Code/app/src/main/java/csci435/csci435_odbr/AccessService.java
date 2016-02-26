939622ced9e9d73b1c18e5fca942b314bacac74d
bwotten <bwotten@email.wme.edu>
2016-02-25 19:00:52 -0500

    Semi screenshot functionaility

--- Code/app/src/main/java/csci435/csci435_odbr/AccessService.java
+++ Code/app/src/main/java/csci435/csci435_odbr/AccessService.java
@@ -6,6 +6,9 @@ import android.view.accessibility.AccessibilityEvent;
 import android.widget.Toast;
 import android.content.Intent;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 /**
  * Created by Brendan Otten on 2/17/2016.
  * resource: http://developer.android.com/guide/topics/ui/accessibility/services.html
@@ -37,6 +40,12 @@ public class AccessService extends AccessibilityService {
 
             BugReport.getInstance().getUserEvents().add(new Events(event));
             //take snapshot
+
+            try {
+                takeScreenShot();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             //}
         }
     }
@@ -50,5 +59,24 @@ public class AccessService extends AccessibilityService {
     public boolean onUnbind(Intent intent) {
         Log.v("AccessService", "RIP AccessService");
         return super.onUnbind(intent);
+    }
+
+    public void takeScreenShot() throws Exception {
+        try{
+            Log.v("AccessibilityScreenshot", "Success");
+            Process su = Runtime.getRuntime().exec("su");
+            DataOutputStream outputStream = new DataOutputStream(su.getOutputStream());
+
+            outputStream.writeBytes("screencap -p " + "/storage/sdcard/events.png");
+            outputStream.flush();
+
+            outputStream.writeBytes("exit\n");
+            outputStream.flush();
+            su.waitFor();
+        }catch(IOException e){
+            throw new Exception(e);
+        }catch(InterruptedException e){
+            throw new Exception(e);
+        }
     }
 }
