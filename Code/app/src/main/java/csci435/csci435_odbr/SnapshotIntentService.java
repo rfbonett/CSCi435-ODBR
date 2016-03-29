6c6d2a6c70dfae75ea489cc7b4be7a5f9d4def46
bwotten <bwotten@email.wme.edu>
2016-03-29 19:05:23 -0400

    I lied

--- Code/app/src/main/java/csci435/csci435_odbr/SnapshotIntentService.java
+++ Code/app/src/main/java/csci435/csci435_odbr/SnapshotIntentService.java
@@ -29,11 +29,7 @@ public class SnapshotIntentService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        //Take screenshot, allows us to define other scenarios just incase we need to here
-
-        //implemented for getEvent
         Bundle extras = intent.getExtras();
-        long timestamp = 0;
         if (Environment.MEDIA_MOUNTED.equals(Environment
                 .getExternalStorageState())) {
             File sdCard = Environment.getExternalStorageDirectory();
@@ -45,6 +41,7 @@ public class SnapshotIntentService extends IntentService {
                 sh = Runtime.getRuntime().exec("su", null, null);
                 os = sh.getOutputStream();
                 Globals.time_last_event = System.currentTimeMillis();
+                Globals.screenshot_index = 0;
                 writeBytes();
             } catch (Exception e) {
                 e.printStackTrace();
@@ -58,54 +55,12 @@ public class SnapshotIntentService extends IntentService {
 
     }
 
-
-    private void takeScreenShot(int i) {
-        Log.v("Snapshot", "Started");
-
-        //Run SU process here, we are in background thread.
-        //Log.v("Screenshot", "Screenshot async occuring");
-        long timestamp = 0;
-        if (Environment.MEDIA_MOUNTED.equals(Environment
-                .getExternalStorageState())) {
-            File sdCard = Environment.getExternalStorageDirectory();
-            File directory = new File(sdCard.getAbsolutePath() + "/ScreenShots");
-            directory.mkdirs();
-
-            filename = "screenshot" + Globals.screenshot_index + ".png";
-            try {
-                sh = Runtime.getRuntime().exec("su", null, null);
-                os = sh.getOutputStream();
-                os.write(("/system/bin/screencap -p " + "/sdcard/ScreenShots/" + filename +"\n").getBytes("ASCII"));
-                os.flush();
-
-                timestamp = System.currentTimeMillis();
-                Screenshots screenshots = new Screenshots(filename, timestamp);
-                BugReport.getInstance().addPotentialScreenshot(screenshots);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-
-        } else {
-            Log.v("Screenshot", "ERROR");
-
-        }
-        //Tell program to restore the widget
-        Intent localIntent = new Intent("csci435.csci435_odbr.SnapshotIntentService.send").putExtra("timestamp", timestamp);
-        localIntent.putExtra("filename", filename);
-        // Broadcasts the Intent to receivers in this app.
-        LocalBroadcastManager.getInstance(this).sendBroadcast(localIntent);
-        //Globals.screenshot = 0;
-    }
-
     public static void writeBytes(){
         try {
             String filename = "screenshot" + Globals.screenshot_index + ".png";
             os.write(("/system/bin/screencap -p " + "/sdcard/ScreenShots/" + filename + "\n").getBytes("ASCII"));
             os.flush();
-            long timestamp = System.currentTimeMillis();
-            Screenshots screenshots = new Screenshots(filename, timestamp);
-            BugReport.getInstance().addPotentialScreenshot(screenshots);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
