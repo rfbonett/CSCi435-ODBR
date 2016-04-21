fc73d881dfa2be1f3bc0cdf42c215b68d4240458
bwotten <bwotten@email.wme.edu>
2016-04-20 22:27:42 -0400

    Added a replay feature with actual functionality, seems to work ok, more testing might be necessary though

--- Code/app/src/main/java/csci435/csci435_odbr/replayService.java
+++ Code/app/src/main/java/csci435/csci435_odbr/replayService.java
@@ -5,10 +5,17 @@ import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
 /**
  * Created by Brendan Otten on 4/20/2016.
  */
 public class ReplayService extends IntentService {
+
+    Process su_replay;
+    OutputStream os;
+    InputStream is;
 
     public ReplayService() {
         super("ReplayService");
@@ -19,14 +26,45 @@ public class ReplayService extends IntentService {
 
         //we're going to have to do an SU thing here, but for now, lets just log something every 10 seconds
 
-        for(int i = 0; i < 10; i++){
-            try {
-                Log.v("Replay", "Time: " + System.currentTimeMillis());
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        try {
+            //we need to iterate through the events list
+            for(int i = 0; i < BugReport.getInstance().getUserEvents().size(); i++){
+                //For each event in the list we need to send that to the su, so:
+                su_replay = Runtime.getRuntime().exec("su", null, null);
+                os = su_replay.getOutputStream();
+                String cmd = "";
+                Events e = BugReport.getInstance().getUserEvents().get(i);
+                if(e.getGetEvent().get_type() == 0){
+                    //we have a click
+                    int [] coords = e.getGetEvent().get_coords().get(e.getGetEvent().get_coords().size() - 1);
+                    cmd = "input tap " + coords[0] + " " + coords[1];
+                }
+                else if(e.getGetEvent().get_type() == 1){
+                    //we have a long click
+                    Float duration = e.getGetEvent().get_duration();
+                    int [] coords = e.getGetEvent().get_coords().get(e.getGetEvent().get_coords().size() - 1);
+                    cmd = "input swipe " + coords[0] + " " + coords[1] + " " + coords[0] + " " + coords[1] + " " + (int)(duration * 1000);
+                }
+                else if(e.getGetEvent().get_type() == 1){
+                    //we have a swipe
+                    Float duration = e.getGetEvent().get_duration();
+                    int [] start_coords = e.getGetEvent().get_coords().get(0);
+                    int [] final_coords = e.getGetEvent().get_coords().get(e.getGetEvent().get_coords().size() - 1);
+                    cmd = "input swipe " + start_coords[0] + " " + start_coords[1] + " " + final_coords[0] + " " + final_coords[1] + " " + (int)(duration * 1000);
+
+                }
+
+                os.write((cmd + "\n").getBytes("ASCII"));
+                os.flush();
+                os.write(("exit\n").getBytes("ASCII"));
+                os.flush();
+                os.close();
+
+                su_replay.waitFor();
+
             }
-        }
+        } catch(Exception e){}
+
 
         Intent record_intent = new Intent(this, RecordActivity.class);
         record_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
