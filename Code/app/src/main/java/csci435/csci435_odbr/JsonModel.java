36f1b93c0e0b610cd1ab47d4ffbb1863c8d80acb
dhpark <dhpark01@email.wm.edu>
2016-04-21 23:47:39 -0400

    cleaned up json model code, refactoring and added a tojson call

--- Code/app/src/main/java/csci435/csci435_odbr/JsonModel.java
+++ Code/app/src/main/java/csci435/csci435_odbr/JsonModel.java
@@ -4,18 +4,33 @@ package csci435.csci435_odbr;
  * Created by danielpark on 4/21/16.
  */
 public class JsonModel {
-    private String device_type = "";
-    private String os_version = "";
-    private String app_name = "";
-    private String app_version = "";
-    private String title = BugReport.getInstance().getTitle();
-    private String name = BugReport.getInstance().getReporterName();
-    private String description_desired_outcome = BugReport.getInstance().getDesiredOutcome();
-    private String description_actual_outcome = BugReport.getInstance().getActualOutcome();
-    private double report_start_time = 11;
-    private double report_end_time = 11;
-    private String accelerometer_stream = "";
-    private String gyroscope_stream = "";
-    private String events = "";
+    private String device_type;
+    private int os_version;
+    private String app_name;
+    private String app_version;
+    private String title;
+    private String name;
+    private String description_desired_outcome;
+    private String description_actual_outcome;
+    private double report_start_time;
+    private double report_end_time;
+    private String accelerometer_stream;
+    private String gyroscope_stream;
+    private String events;
 
+    public void Java_toJson() {
+        os_version = android.os.Build.VERSION.SDK_INT;
+        device_type = android.os.Build.MODEL;
+        app_name = "";
+        app_version = "";
+        title = BugReport.getInstance().getTitle();
+        name = BugReport.getInstance().getReporterName();
+        description_desired_outcome = BugReport.getInstance().getDesiredOutcome();
+        description_actual_outcome = BugReport.getInstance().getActualOutcome();
+        report_start_time = 11;
+        report_end_time = 11;
+        accelerometer_stream = "";
+        gyroscope_stream = "";
+        events = "";
+    }
 }
