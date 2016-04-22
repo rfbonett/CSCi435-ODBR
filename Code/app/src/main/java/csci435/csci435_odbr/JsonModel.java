7d9c1e9674ade8d630710cb3ca4a5d0036ea035a
dhpark <dhpark01@email.wm.edu>
2016-04-21 21:43:43 -0400

    empty variables

--- Code/app/src/main/java/csci435/csci435_odbr/JsonModel.java
+++ Code/app/src/main/java/csci435/csci435_odbr/JsonModel.java
@@ -5,7 +5,16 @@ package csci435.csci435_odbr;
  */
 public class JsonModel {
     private String device_type = "";
-    private String OS_Version = "";
-    private String desiredOutcome = "";
-    private String actualOutcome = "";
+    private String os_version = "";
+    private String app_name = "";
+    private String app_version = "";
+    private String title = "";
+    private String name = "";
+    private String description = "";
+    private double report_start_time = 11;
+    private double report_end_time = 11;
+    private String accelerometer_stream = "";
+    private String gyroscope_stream = "";
+    private String events = "";
+
 }
