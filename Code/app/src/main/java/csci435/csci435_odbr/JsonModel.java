98518d15fe8c088443388e89e258715a8ac139b5
dhpark <dhpark01@email.wm.edu>
2016-04-21 21:51:48 -0400

    fleshed out os and device fields

--- Code/app/src/main/java/csci435/csci435_odbr/JsonModel.java
+++ Code/app/src/main/java/csci435/csci435_odbr/JsonModel.java
@@ -4,8 +4,9 @@ package csci435.csci435_odbr;
  * Created by danielpark on 4/21/16.
  */
 public class JsonModel {
-    private String device_type = "";
-    private String os_version = "";
+
+    private String device_type = "Device: " + android.os.Build.DEVICE;
+    private String os_version = "OS Version: "+ System.getProperty("os.version");
     private String app_name = "";
     private String app_version = "";
     private String title = "";
@@ -15,6 +16,13 @@ public class JsonModel {
     private double report_end_time = 11;
     private String accelerometer_stream = "";
     private String gyroscope_stream = "";
+
     private String events = "";
+
+    class Eventset {
+        public String screenshots;
+        public String event_start;
+        public String event_end;
+    }
 
 }
